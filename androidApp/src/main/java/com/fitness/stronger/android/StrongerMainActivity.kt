b27143a3c28279e2c2a496bcfe704cab093a9246
kingjinho <jinhoshin319@gmail.com>
2023-11-07 20:15:11 +0900

    rename: add "Stronger" as prefix to avoid meaningless file name

--- androidApp/src/main/java/com/fitness/stronger/android/StrongerMainActivity.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/StrongerMainActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fitness.stronger.Greeting
 
-class MainActivity : ComponentActivity() {
+class StrongerMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            StrongerApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -34,7 +36,7 @@ fun GreetingView(text: String) {
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
+    StrongerApplicationTheme {
         GreetingView("Hello, Android!")
     }
 }
