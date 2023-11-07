cd270d487f3b3fa52d48eb4b53f6bcf8e1645287
kingjinho <jinhoshin319@gmail.com>
2023-11-07 21:23:37 +0900

    style: set typography values, from jetpack compose sample

--- androidApp/src/main/java/com/fitness/stronger/android/StrongerTheme.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/StrongerTheme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -34,11 +35,125 @@ fun StrongerApplicationTheme(
         )
     }
     val typography = Typography(
+        displayLarge = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 57.sp,
+            lineHeight = 64.sp,
+            letterSpacing = (-0.25).sp,
+        ),
+
+        displayMedium = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 45.sp,
+            lineHeight = 52.sp,
+            letterSpacing = 0.sp,
+        ),
+
+        displaySmall = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 36.sp,
+            lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+        ),
+
+        headlineLarge = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 32.sp,
+            lineHeight = 40.sp,
+            letterSpacing = 0.sp,
+        ),
+
+        headlineMedium = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 28.sp,
+            lineHeight = 36.sp,
+            letterSpacing = 0.sp,
+        ),
+
+        headlineSmall = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 24.sp,
+            lineHeight = 32.sp,
+            letterSpacing = 0.sp,
+        ),
+
+        titleLarge = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 22.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp,
+        ),
+
+        titleMedium = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W500,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.15.sp,
+        ),
+
+        titleSmall = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.1.sp,
+        ),
+
+        labelLarge = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.1.sp,
+        ),
+
+        labelMedium = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W500,
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.5.sp,
+        ),
+
+        labelSmall = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W500,
+            fontSize = 11.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.5.sp,
+        ),
+
+        bodyLarge = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.5.sp,
+        ),
+
         bodyMedium = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
-        )
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.25.sp,
+        ),
+
+        bodySmall = TextStyle(
+            fontFamily = FontFamily(Font(R.font.sf_pro)),
+            fontWeight = FontWeight.W400,
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.4.sp,
+        ),
     )
     val shapes = Shapes(
         small = RoundedCornerShape(4.dp),
