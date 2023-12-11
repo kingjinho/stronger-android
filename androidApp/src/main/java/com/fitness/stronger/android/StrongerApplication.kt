015373f823485fb5ed51c560c3585b1765179757
kingjinho <jinhoshin319@gmail.com>
2023-12-11 23:04:31 +0900

    feat: add HiltAndroidApp annotation

--- androidApp/src/main/java/com/fitness/stronger/android/StrongerApplication.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/StrongerApplication.kt
@@ -1,7 +1,9 @@
 package com.fitness.stronger.android
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class StrongerApplication : Application() {
 
     override fun onCreate() {
