cbac1b712386ae703af7bc522761ba16b9087cbf
kingjinho <jinhoshin319@gmail.com>
2024-01-03 23:25:00 +0900

    feat: add zstack for setting default background color

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
-		}
+            ZStack {
+                Color(hexCode: "#1C1C1C")
+                    .ignoresSafeArea(.all)
+                
+                ContentView()
+            }
+        }
 	}
 }
