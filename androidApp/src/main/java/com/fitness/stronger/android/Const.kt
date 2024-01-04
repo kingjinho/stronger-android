97ea79c1c680c69617260517ace14678293c74fb
kingjinho <jinhoshin319@gmail.com>
2024-01-04 20:34:02 +0900

    feat: 공통화 상수 추가

---
+++ androidApp/src/main/java/com/fitness/stronger/android/Const.kt
@@ -0,0 +1,9 @@
+package com.fitness.stronger.android
+
+object Const {
+    const val PREF_NAME = "STRONGER"
+    const val PREF_KEY_IS_INITIAL_OPEN = "is_initial_open"
+    const val ROUTE_ONBOARDING = "onboarding"
+    const val ROUTE_MAIN = "main"
+    const val ROUTE_CAMERA_VIEW = "camera_view"
+}
