3a27380c42fa368837ab2276e3c5d4c48f6c69cc
kingjinho <jinhoshin319@gmail.com>
2024-01-12 17:38:13 +0900

    feat: add data class for line drawing

---
+++ androidApp/src/main/java/com/fitness/stronger/android/data/LineData.kt
@@ -0,0 +1,11 @@
+package com.fitness.stronger.android.data
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class LineData(
+    val start: Offset,
+    val end: Offset,
+    val strokeWidth: Dp = 20.dp
+)
