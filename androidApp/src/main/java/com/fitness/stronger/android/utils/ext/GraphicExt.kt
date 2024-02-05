9c22b514a6e02e375f2dac664dbbfd822583b5e4
kingjinho <jinhoshin319@gmail.com>
2024-02-05 19:23:04 +0900

    refactor: commented out to reflect unused

--- androidApp/src/main/java/com/fitness/stronger/android/utils/ext/GraphicExt.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/utils/ext/GraphicExt.kt
@@ -1,16 +1,16 @@
-package com.fitness.stronger.android.utils.ext
-
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import com.fitness.stronger.android.data.LineData
-
-fun DrawScope.drawLine(data: LineData) {
-    drawLine(
-        color = Color(0xFFBB86FC),
-        start = data.start,
-        end = data.end,
-        strokeWidth = data.strokeWidth.value,
-        cap = StrokeCap.Round
-    )
-}
+//package com.fitness.stronger.android.utils.ext
+//
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.StrokeCap
+//import androidx.compose.ui.graphics.drawscope.DrawScope
+//import com.fitness.stronger.android.data.LineData
+//
+//fun DrawScope.drawLine(data: LineData) {
+//    drawLine(
+//        color = Color(0xFFBB86FC),
+//        start = data.start,
+//        end = data.end,
+//        strokeWidth = data.strokeWidth.value,
+//        cap = StrokeCap.Round
+//    )
+//}
