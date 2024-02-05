19982299ebbfee31d3a265a9b3bd7b90417e7554
kingjinho <jinhoshin319@gmail.com>
2024-02-05 19:23:28 +0900

    refactor: commented out to reflect unused

--- androidApp/src/main/java/com/fitness/stronger/android/StrongerMainActivity.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/StrongerMainActivity.kt
@@ -3,13 +3,66 @@ package com.fitness.stronger.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.fragment
+import com.fitness.stronger.android.databinding.ActivityMainStrongerBinding
+import com.fitness.stronger.android.screens.compose.navigation.Route
+import com.fitness.stronger.android.screens.view_system.main.ScreenMainView
+import com.fitness.stronger.android.screens.view_system.main.camera.ScreenCameraView
+import com.fitness.stronger.android.screens.view_system.onboarding.ScreenOnboardingView
 import com.fitness.stronger.android.utils.ext.getSharedPref
 
 class StrongerMainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainStrongerBinding? = null
+    private val binding: ActivityMainStrongerBinding
+        get() = _binding!!
+
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment)
+            .navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_stronger)
+        _binding = ActivityMainStrongerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        createNavGraph()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun createNavGraph() {
+        val startDestination = getStartDestinationByInitialOpenStatus()
+        navController.graph = navController.createGraph(
+            startDestination
+        ) {
+            fragment<ScreenMainView>(Route.Main.name)
+            fragment<ScreenOnboardingView>(Route.Onboarding.name)
+            fragment<ScreenCameraView>(Route.CameraView.name)
+        }
+    }
+
+    private fun isInitialAppOpen() =
+        application.getSharedPref().getBoolean(Const.PREF_KEY_IS_INITIAL_OPEN, true)
+
+    private fun setInitialAppOpenToFalse() =
+        application.getSharedPref().edit(true) {
+            putBoolean(Const.PREF_KEY_IS_INITIAL_OPEN, false)
+        }
+
+    private fun getStartDestinationByInitialOpenStatus(): String {
+        return if (isInitialAppOpen()) {
+            setInitialAppOpenToFalse()
+            Route.Onboarding.name
+        } else {
+            Route.Main.name
+        }
     }
 
 //    @OptIn(ExperimentalPermissionsApi::class)
@@ -70,12 +123,4 @@ class StrongerMainActivity : AppCompatActivity() {
 //            }
 //        }
 //    }
-
-    private fun isInitialAppOpen() =
-        application.getSharedPref().getBoolean(Const.PREF_KEY_IS_INITIAL_OPEN, true)
-
-    private fun setInitialAppOpenToFalse() =
-        application.getSharedPref().edit(true) {
-            putBoolean(Const.PREF_KEY_IS_INITIAL_OPEN, false)
-        }
 }
--- androidApp/src/main/java/com/fitness/stronger/android/StrongerTheme.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/StrongerTheme.kt
@@ -1,174 +1,174 @@
-package com.fitness.stronger.android
+//package com.fitness.stronger.android
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
-@Composable
-fun StrongerApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        darkColorScheme(
-            primary = Color(0xFFBB86FC),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
-        )
-    } else {
-        darkColorScheme(
-            primary = Color(0xFFBB86FC),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
-        )
-//        lightColorScheme(
-//            primary = Color(0xFF6200EE),
+//import androidx.compose.foundation.isSystemInDarkTheme
+//import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Shapes
+//import androidx.compose.material3.Typography
+//import androidx.compose.material3.darkColorScheme
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.text.TextStyle
+//import androidx.compose.ui.text.font.Font
+//import androidx.compose.ui.text.font.FontFamily
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//
+//@Composable
+//fun StrongerApplicationTheme(
+//    darkTheme: Boolean = isSystemInDarkTheme(),
+//    content: @Composable () -> Unit
+//) {
+//    val colors = if (darkTheme) {
+//        darkColorScheme(
+//            primary = Color(0xFFBB86FC),
 //            secondary = Color(0xFF03DAC5),
 //            tertiary = Color(0xFF3700B3)
 //        )
-    }
-    val typography = Typography(
-        displayLarge = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 57.sp,
-            lineHeight = 64.sp,
-            letterSpacing = (-0.25).sp,
-        ),
-
-        displayMedium = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 45.sp,
-            lineHeight = 52.sp,
-            letterSpacing = 0.sp,
-        ),
-
-        displaySmall = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 36.sp,
-            lineHeight = 44.sp,
-            letterSpacing = 0.sp,
-        ),
-
-        headlineLarge = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 32.sp,
-            lineHeight = 40.sp,
-            letterSpacing = 0.sp,
-        ),
-
-        headlineMedium = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 28.sp,
-            lineHeight = 36.sp,
-            letterSpacing = 0.sp,
-        ),
-
-        headlineSmall = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 24.sp,
-            lineHeight = 32.sp,
-            letterSpacing = 0.sp,
-        ),
-
-        titleLarge = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 22.sp,
-            lineHeight = 28.sp,
-            letterSpacing = 0.sp,
-        ),
-
-        titleMedium = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W500,
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.15.sp,
-        ),
-
-        titleSmall = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W500,
-            fontSize = 14.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.1.sp,
-        ),
-
-        labelLarge = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W500,
-            fontSize = 14.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.1.sp,
-        ),
-
-        labelMedium = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W500,
-            fontSize = 12.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.5.sp,
-        ),
-
-        labelSmall = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W500,
-            fontSize = 11.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.5.sp,
-        ),
-
-        bodyLarge = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.5.sp,
-        ),
-
-        bodyMedium = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 14.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.25.sp,
-        ),
-
-        bodySmall = TextStyle(
-            fontFamily = FontFamily(Font(R.font.sf_pro)),
-            fontWeight = FontWeight.W400,
-            fontSize = 12.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.4.sp,
-        ),
-    )
-    val shapes = Shapes(
-        small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp)
-    )
-
-    MaterialTheme(
-        colorScheme = colors,
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
-}
+//    } else {
+//        darkColorScheme(
+//            primary = Color(0xFFBB86FC),
+//            secondary = Color(0xFF03DAC5),
+//            tertiary = Color(0xFF3700B3)
+//        )
+////        lightColorScheme(
+////            primary = Color(0xFF6200EE),
+////            secondary = Color(0xFF03DAC5),
+////            tertiary = Color(0xFF3700B3)
+////        )
+//    }
+//    val typography = Typography(
+//        displayLarge = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 57.sp,
+//            lineHeight = 64.sp,
+//            letterSpacing = (-0.25).sp,
+//        ),
+//
+//        displayMedium = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 45.sp,
+//            lineHeight = 52.sp,
+//            letterSpacing = 0.sp,
+//        ),
+//
+//        displaySmall = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 36.sp,
+//            lineHeight = 44.sp,
+//            letterSpacing = 0.sp,
+//        ),
+//
+//        headlineLarge = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 32.sp,
+//            lineHeight = 40.sp,
+//            letterSpacing = 0.sp,
+//        ),
+//
+//        headlineMedium = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 28.sp,
+//            lineHeight = 36.sp,
+//            letterSpacing = 0.sp,
+//        ),
+//
+//        headlineSmall = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 24.sp,
+//            lineHeight = 32.sp,
+//            letterSpacing = 0.sp,
+//        ),
+//
+//        titleLarge = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 22.sp,
+//            lineHeight = 28.sp,
+//            letterSpacing = 0.sp,
+//        ),
+//
+//        titleMedium = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W500,
+//            fontSize = 16.sp,
+//            lineHeight = 24.sp,
+//            letterSpacing = 0.15.sp,
+//        ),
+//
+//        titleSmall = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W500,
+//            fontSize = 14.sp,
+//            lineHeight = 20.sp,
+//            letterSpacing = 0.1.sp,
+//        ),
+//
+//        labelLarge = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W500,
+//            fontSize = 14.sp,
+//            lineHeight = 20.sp,
+//            letterSpacing = 0.1.sp,
+//        ),
+//
+//        labelMedium = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W500,
+//            fontSize = 12.sp,
+//            lineHeight = 16.sp,
+//            letterSpacing = 0.5.sp,
+//        ),
+//
+//        labelSmall = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W500,
+//            fontSize = 11.sp,
+//            lineHeight = 16.sp,
+//            letterSpacing = 0.5.sp,
+//        ),
+//
+//        bodyLarge = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 16.sp,
+//            lineHeight = 24.sp,
+//            letterSpacing = 0.5.sp,
+//        ),
+//
+//        bodyMedium = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 14.sp,
+//            lineHeight = 20.sp,
+//            letterSpacing = 0.25.sp,
+//        ),
+//
+//        bodySmall = TextStyle(
+//            fontFamily = FontFamily(Font(R.font.sf_pro)),
+//            fontWeight = FontWeight.W400,
+//            fontSize = 12.sp,
+//            lineHeight = 16.sp,
+//            letterSpacing = 0.4.sp,
+//        ),
+//    )
+//    val shapes = Shapes(
+//        small = RoundedCornerShape(4.dp),
+//        medium = RoundedCornerShape(4.dp),
+//        large = RoundedCornerShape(0.dp)
+//    )
+//
+//    MaterialTheme(
+//        colorScheme = colors,
+//        typography = typography,
+//        shapes = shapes,
+//        content = content
+//    )
+//}
