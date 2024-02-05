39b4bdf5425b605c277d1d9d3b22496d7a1a0e58
kingjinho <jinhoshin319@gmail.com>
2024-02-05 19:23:10 +0900

    refactor: commented out to reflect unused

--- androidApp/src/main/java/com/fitness/stronger/android/screens/compose/dialog/BottomSheetDialog.kt
+++ androidApp/src/main/java/com/fitness/stronger/android/screens/compose/dialog/BottomSheetDialog.kt
@@ -1,14 +1,14 @@
-package com.fitness.stronger.android.screens.compose.dialog
-
-import androidx.activity.compose.BackHandler
-import androidx.compose.runtime.Composable
-
-@Composable
-fun BottomSheetDialog(
-    content: @Composable () -> Unit
-) {
-    BackHandler {
-
-    }
-    content()
-}
+//package com.fitness.stronger.android.screens.compose.dialog
+//
+//import androidx.activity.compose.BackHandler
+//import androidx.compose.runtime.Composable
+//
+//@Composable
+//fun BottomSheetDialog(
+//    content: @Composable () -> Unit
+//) {
+//    BackHandler {
+//
+//    }
+//    content()
+//}
